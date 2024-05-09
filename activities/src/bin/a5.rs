a8411436f944e13638f118c051b6f70985e5ac3d
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-05-09 07:03:57 +0100

    new changes

--- activities/src/bin/a5.rs
+++ activities/src/bin/a5.rs
@@ -9,4 +9,18 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+fn main() {
+    // * Use a mutable integer variable
+    let mut i: i32 = 1;
+    // * Use a loop statement
+    loop {
+    // * Print the variable within the loop statement
+        println!("{:?}", i);
+    // * Use break to exit the loop
+        if i == 4 {
+            break;
+        }
+        i = i + 1;
+
+    }
+}
