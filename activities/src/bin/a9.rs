924f5689076cc6280737ab1bac85bc2c97f575a7
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-05-09 10:26:54 +0100

    tuples

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -8,5 +8,17 @@
 // * Use a function that returns a tuple
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
+fn coord() -> (i32, i32){
+    (4, 3)
+}
 
-fn main() {}
+fn main() {
+    let (_x, y) = coord();
+    if y > 5 {
+        println!("y is greater than 5");
+    } else if y == 5 {
+        println!("y is equal to 5");
+    } else {
+        println!("y is less than 5")
+    }
+}
