e1391021b7ea4b19918638a0b46e89a9d9800009
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-05-10 16:34:14 +0100

    vectors and for..in loops

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -10,4 +10,20 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    // * Use a vector to store 4 numbers
+    let numbers = vec![10, 20, 30, 40];
+
+    // * Iterate through the vector using a for..in loop
+    for number in &numbers {
+        // * Determine whether to print the number or print "thirty" inside the loop
+        if *number == 30 {
+            println!("thirty");
+        } else {
+            println!("{:?}", number);
+        }
+    }
+
+    // * Use the .len() function to print the number of elements in a vector
+    println!("{:?}", numbers.len());
+}
